bad84ceed1514b903fc16f54f184d7d166e63e04
jaydonhansen <jaydon.hansen@uq.net.au>
2019-10-10 15:05:26 +1000

    Basic Seurat PCA plots for 3-cell lines

--- scripts/processing.R
+++ scripts/processing.R
@@ -5,9 +5,19 @@ library(SingleCellExperiment)
 setwd("~/advanced-bioinformatics/")
 load("data/sincell_with_class.RData")
 
-sce_10x_seurat <- sce_sc_10x_qc %>% as.Seurat(counts = "counts", data = "logcounts") %>%
+seurat_10x_qc <- sce_sc_10x_qc %>% as.Seurat(counts = "counts", data = "logcounts") %>%
     FindVariableFeatures() %>% ScaleData() %>% RunPCA()
 
-p1 <- DimPlot(sce_10x_seurat, reduction = "pca", group.by = "cell_line")
+seurat_CELseq2_qc <- sce_sc_CELseq2_qc %>% as.Seurat(counts = "counts", data = "logcounts") %>%
+    FindVariableFeatures() %>% ScaleData() %>% RunPCA()
+
+seurat_Dropseq_qc <- sce_sc_Dropseq_qc %>% as.Seurat(counts = "counts", data = "logcounts") %>%
+    FindVariableFeatures() %>% ScaleData() %>% RunPCA()
+
+p1 <- DimPlot(seurat_10x_qc, reduction = "pca", group.by = "cell_line")
+p2 <- DimPlot(seurat_CELseq2_qc, reduction = "pca", group.by = "cell_line")
+p3 <- DimPlot(seurat_Dropseq_qc, reduction = "pca", group.by = "cell_line")
 
 p1
+p2
+p3
