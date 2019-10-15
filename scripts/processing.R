ade00a22ed621b6e641b3f184d9296c08ecafa61
jaydonhansen <jaydon.hansen@uq.net.au>
2019-10-15 20:13:52 +1000

    t-SNE plots plus some 5-cell stuff

--- scripts/processing.R
+++ scripts/processing.R
@@ -4,42 +4,127 @@ library(SingleCellExperiment)
 
 setwd("~/advanced-bioinformatics/")
 load("data/sincell_with_class.RData")
+load("data/sincell_with_class_5cl.RData")
 
-# Pipe the 10x dataset through the regular PCA pipeline in Seurat
-seurat_10x_qc <- sce_sc_10x_qc %>% 
+# Pipe the 10x datasets through the regular PCA/t-SNE pipeline in Seurat
+seurat_10x_3cl <- sce_sc_10x_qc %>% 
     as.Seurat(counts = "counts", data = "logcounts") %>%
-    FindVariableFeatures() %>% ScaleData() %>% RunPCA()
+    FindVariableFeatures() %>% ScaleData() %>% RunPCA() %>% RunTSNE
 
-rm(sce_sc_10x_qc) # Save memory
-
-# Pipe the CELLseq2 dataset through the regular PCA pipeline in Seurat
-seurat_CELseq2_qc <- sce_sc_CELseq2_qc %>%
+# Pipe the 10x datasets through the regular PCA/t-SNE pipeline in Seurat
+seurat_10x_5cl <- sce_sc_10x_5cl_qc %>% 
     as.Seurat(counts = "counts", data = "logcounts") %>%
-    FindVariableFeatures() %>% ScaleData() %>% RunPCA()
+    FindVariableFeatures() %>% ScaleData() %>% RunPCA() %>% RunTSNE
 
-rm(sce_sc_CELseq2_qc) # Save memory
+rm(sce_sc_10x_qc, sce_sc_10x_5cl_qc) # Save memory
 
-# Pipe the Dropseq dataset through the regular PCA pipeline in Seurat
-seurat_Dropseq_qc <- sce_sc_Dropseq_qc %>% 
+
+# Pipe the CELLseq2 datasets through the regular PCA/t-SNE pipeline in Seurat
+seurat_CELseq2_3cl <- sce_sc_CELseq2_qc %>%
     as.Seurat(counts = "counts", data = "logcounts") %>%
-    FindVariableFeatures() %>% ScaleData() %>% RunPCA()
+    FindVariableFeatures() %>% ScaleData() %>% RunPCA() %>% RunTSNE
+
+seurat_CELseq2_5cl_p1 <- sc_Celseq2_5cl_p1 %>% 
+    as.Seurat(counts = "counts", data = "logcounts") %>%
+    FindVariableFeatures() %>% ScaleData() %>% RunPCA() %>% RunTSNE
+
+seurat_CELseq2_5cl_p2 <- sc_Celseq2_5cl_p2 %>% 
+    as.Seurat(counts = "counts", data = "logcounts") %>%
+    FindVariableFeatures() %>% ScaleData() %>% RunPCA() %>% RunTSNE
+
+seurat_CELseq2_5cl_p3 <- sc_Celseq2_5cl_p3 %>% 
+    as.Seurat(counts = "counts", data = "logcounts") %>%
+    FindVariableFeatures() %>% ScaleData() %>% RunPCA() %>% RunTSNE
+
+rm(sce_sc_CELseq2_qc, sc_Celseq2_5cl_p1, sc_Celseq2_5cl_p2, sc_Celseq2_5cl_p3) # Save memory
+
+# Pipe the Dropseq datasets through the regular PCA/t-SNE pipeline in Seurat
+seurat_Dropseq_3cl <- sce_sc_Dropseq_qc %>% 
+    as.Seurat(counts = "counts", data = "logcounts") %>%
+    FindVariableFeatures() %>% ScaleData() %>% RunPCA() %>% RunTSNE
 
 rm(sce_sc_Dropseq_qc) # Save memory
 
-# Some PCA plots
-plot_10x_qc <- DimPlot(seurat_10x_qc, reduction = "pca", group.by = "cell_line")
-plot_CELseq2_qc <- DimPlot(seurat_CELseq2_qc, reduction = "pca", group.by = "cell_line")
-plot_Dropseq_qc <- DimPlot(seurat_Dropseq_qc, reduction = "pca", group.by = "cell_line")
+# PCA/t-SNE plots for 10x
+pca_plot_10x_3cl <- DimPlot(seurat_10x_3cl, reduction = "pca", group.by = "cell_line")
+pca_plot_10x_5cl <- DimPlot(seurat_10x_5cl, reduction = "pca", group.by = "cell_line")
 
-# Output the plots into images
-png("output/3-cell-line-10x-pca-seurat.png", height = 1000, width = 1000)
-    plot_10x_qc
+tsne_plot_10x_3cl <- DimPlot(seurat_10x_3cl, reduction = "tsne", group.by = "cell_line")
+tsne_plot_10x_5cl <- DimPlot(seurat_10x_5cl, reduction = "tsne", group.by = "cell_line")
+
+# PCA/t-SNE plots for CELseq2
+pca_plot_CELseq2_3cl <- DimPlot(seurat_CELseq2_3cl, reduction = "pca", group.by = "cell_line")
+pca_plot_CELseq2_5cl_p1 <- DimPlot(seurat_CELseq2_5cl_p1, reduction = "pca", group.by = "cell_line")
+pca_plot_CELseq2_5cl_p2 <- DimPlot(seurat_CELseq2_5cl_p2, reduction = "pca", group.by = "cell_line")
+pca_plot_CELseq2_5cl_p3 <- DimPlot(seurat_CELseq2_5cl_p3, reduction = "pca", group.by = "cell_line")
+
+tsne_plot_CELseq2_3cl <- DimPlot(seurat_CELseq2_3cl, reduction = "tsne", group.by = "cell_line")
+tsne_plot_CELseq2_5cl_p1 <- DimPlot(seurat_CELseq2_5cl_p1, reduction = "tsne", group.by = "cell_line")
+tsne_plot_CELseq2_5cl_p2 <- DimPlot(seurat_CELseq2_5cl_p2, reduction = "tsne", group.by = "cell_line")
+tsne_plot_CELseq2_5cl_p3 <- DimPlot(seurat_CELseq2_5cl_p3, reduction = "tsne", group.by = "cell_line")
+
+pca_plot_Dropseq <- DimPlot(seurat_Dropseq_3cl, reduction = "pca", group.by = "cell_line")
+tsne_plot_Dropseq <- DimPlot(seurat_Dropseq_3cl, reduction = "tsne", group.by = "cell_line")
+
+
+# 10x samples
+
+png("output/seurat/seurat-pca-3cl-10x.png", height = 1000, width = 1000)
+    pca_plot_10x_3cl # 3-cell PCA
 dev.off()
 
-png("output/3-cell-line-CELseq2-pca-seurat.png", height = 1000, width = 1000)
-    plot_CELseq2_qc
+png("output/seurat/seurat-tsne-3cl-10x.png", height = 1000, width = 1000)
+    tsne_plot_10x_3cl # 3-cell t-SNE
 dev.off()
 
-png("output/3-cell-line-Dropseq-pca-seurat.png", height = 1000, width = 1000)
-    plot_Dropseq_qc
+png("output/seurat/seurat-pca-5cl-10x.png", height = 1000, width = 1000)
+    pca_plot_10x_5cl # 5-cell PCA
+dev.off()
+
+png("output/seurat/seurat-tsne-5cl-10x.png", height = 1000, width = 1000)
+    tsne_plot_10x_5cl # 5-cell t-SNE
+dev.off()
+
+# CELseq2 samples
+
+png("output/seurat/seurat-pca-3cl-CELseq2.png", height = 1000, width = 1000)
+    pca_plot_CELseq2_3cl # 3-cell PCA
+dev.off()
+
+png("output/seurat/seurat-tsne-3cl-CELseq2.png", height = 1000, width = 1000)
+    tsne_plot_CELseq2_3cl # 3-cell t-SNE
+dev.off()
+
+png("output/seurat/seurat-pca-5cl-p1-CELseq2.png", height = 1000, width = 1000)
+    pca_plot_CELseq2_5cl_p1 # 5-cell p1 PCA
+dev.off()
+
+png("output/seurat/seurat-tsne-5cl-p1-CELseq2.png", height = 1000, width = 1000)
+    tsne_plot_CELseq2_5cl_p1 # 5-cell p1 t-SNE
+dev.off()
+
+png("output/seurat/seurat-pca-5cl-p2-CELseq2.png", height = 1000, width = 1000)
+    pca_plot_CELseq2_5cl_p2 # 5-cell p2 PCA
+dev.off()
+
+png("output/seurat/seurat-tsne-5cl-p2-CELseq2.png", height = 1000, width = 1000)
+    tsne_plot_CELseq2_5cl_p2 # 5-cell p2 t-SNE
+dev.off()
+
+png("output/seurat/seurat-pca-5cl-p3-CELseq2.png", height = 1000, width = 1000)
+    pca_plot_CELseq2_5cl_p3 # 5-cell p3 PCA
+dev.off()
+
+png("output/seurat/seurat-tsne-5cl-p3-CELseq2.png", height = 1000, width = 1000)
+    tsne_plot_CELseq2_5cl_p3 # 5-cell p3 t-SNE
+dev.off()
+
+# Dropseq samples
+
+png("output/seurat/seurat-pca-3cl-Dropseq.png", height = 1000, width = 1000)
+    pca_plot_Dropseq # 3-cell PCA
+dev.off()
+
+png("output/seurat/seurat-tsne-3cl-Dropseq.png", height = 1000, width = 1000)
+    tsne_plot_Dropseq # 3-cell t-SNE
 dev.off()
