048e6030a2f94c751c07a9ef80fcd3b67aea4d00
jaydonhansen <jaydon.hansen@uq.net.au>
2019-10-15 21:25:06 +1000

    10x UMAP done

--- scripts/seurat.R
+++ scripts/seurat.R
@@ -9,12 +9,12 @@ load("data/sincell_with_class_5cl.RData")
 # Pipe the 10x datasets through the regular PCA/t-SNE pipeline in Seurat
 seurat_10x_3cl <- sce_sc_10x_qc %>% 
     as.Seurat(counts = "counts", data = "logcounts") %>%
-    FindVariableFeatures() %>% ScaleData() %>% RunPCA() %>% RunTSNE
+    FindVariableFeatures() %>% ScaleData() %>% RunPCA() %>% RunTSNE %>% RunUMAP(dims = 1:5)
 
 # Pipe the 10x datasets through the regular PCA/t-SNE pipeline in Seurat
 seurat_10x_5cl <- sce_sc_10x_5cl_qc %>% 
     as.Seurat(counts = "counts", data = "logcounts") %>%
-    FindVariableFeatures() %>% ScaleData() %>% RunPCA() %>% RunTSNE
+    FindVariableFeatures() %>% ScaleData() %>% RunPCA() %>% RunTSNE %>% RunUMAP(dims = 1:5)
 
 rm(sce_sc_10x_qc, sce_sc_10x_5cl_qc) # Save memory
 
@@ -22,26 +22,26 @@ rm(sce_sc_10x_qc, sce_sc_10x_5cl_qc) # Save memory
 # Pipe the CELLseq2 datasets through the regular PCA/t-SNE pipeline in Seurat
 seurat_CELseq2_3cl <- sce_sc_CELseq2_qc %>%
     as.Seurat(counts = "counts", data = "logcounts") %>%
-    FindVariableFeatures() %>% ScaleData() %>% RunPCA() %>% RunTSNE
+    FindVariableFeatures() %>% ScaleData() %>% RunPCA() %>% RunTSNE %>% RunUmap(dims = 1:5)
 
 seurat_CELseq2_5cl_p1 <- sc_Celseq2_5cl_p1 %>% 
     as.Seurat(counts = "counts", data = "logcounts") %>%
-    FindVariableFeatures() %>% ScaleData() %>% RunPCA() %>% RunTSNE
+    FindVariableFeatures() %>% ScaleData() %>% RunPCA() %>% RunTSNE %>% RunUmap(dims = 1:5)
 
 seurat_CELseq2_5cl_p2 <- sc_Celseq2_5cl_p2 %>% 
     as.Seurat(counts = "counts", data = "logcounts") %>%
-    FindVariableFeatures() %>% ScaleData() %>% RunPCA() %>% RunTSNE
+    FindVariableFeatures() %>% ScaleData() %>% RunPCA() %>% RunTSNE %>% RunUmap(dims = 1:5)
 
 seurat_CELseq2_5cl_p3 <- sc_Celseq2_5cl_p3 %>% 
     as.Seurat(counts = "counts", data = "logcounts") %>%
-    FindVariableFeatures() %>% ScaleData() %>% RunPCA() %>% RunTSNE
+    FindVariableFeatures() %>% ScaleData() %>% RunPCA() %>% RunTSNE %>% RunUmap(dims = 1:5)
 
 rm(sce_sc_CELseq2_qc, sc_Celseq2_5cl_p1, sc_Celseq2_5cl_p2, sc_Celseq2_5cl_p3) # Save memory
 
 # Pipe the Dropseq datasets through the regular PCA/t-SNE pipeline in Seurat
 seurat_Dropseq_3cl <- sce_sc_Dropseq_qc %>% 
     as.Seurat(counts = "counts", data = "logcounts") %>%
-    FindVariableFeatures() %>% ScaleData() %>% RunPCA() %>% RunTSNE
+    FindVariableFeatures() %>% ScaleData() %>% RunPCA() %>% RunTSNE %>% RunUmap(dims = 1:5)
 
 rm(sce_sc_Dropseq_qc) # Save memory
 
@@ -51,6 +51,10 @@ pca_plot_10x_5cl <- DimPlot(seurat_10x_5cl, reduction = "pca", group.by = "cell_
 
 tsne_plot_10x_3cl <- DimPlot(seurat_10x_3cl, reduction = "tsne", group.by = "cell_line")
 tsne_plot_10x_5cl <- DimPlot(seurat_10x_5cl, reduction = "tsne", group.by = "cell_line")
+
+# UMAP plots for 10x
+umap_plot_10x_3cl <- DimPlot(seurat_10x_3cl, reduction = "umap", group.by = "cell_line")
+umap_plot_10x_5cl <- DimPlot(seurat_10x_5cl, reduction = "umap", group.by = "cell_line")
 
 # PCA/t-SNE plots for CELseq2
 pca_plot_CELseq2_3cl <- DimPlot(seurat_CELseq2_3cl, reduction = "pca", group.by = "cell_line")
@@ -77,6 +81,10 @@ png("output/seurat/seurat-tsne-3cl-10x.png", height = 1000, width = 1000)
     tsne_plot_10x_3cl # 3-cell t-SNE
 dev.off()
 
+png("output/seurat/seurat-umap-3cl-10x.png", height = 1000, width = 1000)
+    umap_plot_10x_3cl # 3-cell UMAP
+dev.off()
+
 png("output/seurat/seurat-pca-5cl-10x.png", height = 1000, width = 1000)
     pca_plot_10x_5cl # 5-cell PCA
 dev.off()
@@ -84,6 +92,11 @@ dev.off()
 png("output/seurat/seurat-tsne-5cl-10x.png", height = 1000, width = 1000)
     tsne_plot_10x_5cl # 5-cell t-SNE
 dev.off()
+
+png("output/seurat/seurat-umap-5cl-10x.png", height = 1000, width = 1000)
+    umap_plot_10x_5cl # 5-cell UMAP
+dev.off()
+
 
 # CELseq2 samples
 
